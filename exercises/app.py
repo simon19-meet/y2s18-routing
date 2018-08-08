e279de960afff8addd307bd196e69bcfc2f9f80a
Simon Deeb <simon19-meet@meet.mit.edu>
2018-08-08 04:11:00 -0700

    annoying lab

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,17 @@
 from databases import *
 from flask import Flask, render_template, url_for
+from model import *
 app = Flask(__name__)
 
+#add_student("hh",3,True)
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return render_template("home.html")
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+
+    stud=query_by_id(student_id)
+    return render_template("student.html",student_id=student_id,student=stud)
 
 app.run(debug=True)
